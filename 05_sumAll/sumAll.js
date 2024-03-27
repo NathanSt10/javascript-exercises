95ba150f41d1b467d1f62d5af7d3c7b72fa15004
NathanSt10 <nstout@uwm.edu>
2024-03-26 20:13:19 -0500

    Completed fifth exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+    if (!Number.isInteger(x) || !Number.isInteger(y)) return "ERROR";
+    if (x < 0 || y < 0) return "ERROR";
+    // assign i to the lower int
+    let i = (x < y) ? x : y;
+    // figure out which int to iterate up to
+    let ceiling = x + y - i;
+    let sum = 0;
+    for (; i <= ceiling; ++i) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
