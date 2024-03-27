5c93c11dba0ce6d10c9831ddb2232b9dafc076dc
NathanSt10 <nstout@uwm.edu>
2024-03-26 21:17:59 -0500

    Completed seventh exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (!Number.isInteger(temp)) return "ERROR";
+  celsius = (temp - 32) * (5/9);
+  celsius = celsius * 10;
+  celsius = Math.round(celsius);
+  celsius = celsius / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  fahrenheit = (temp * (9/5)) + 32;
+  fahrenheit = fahrenheit * 10;
+  fahrenheit = Math.round(fahrenheit);
+  fahrenheit = fahrenheit / 10;
+  return fahrenheit;
 };
 
 // Do not edit below this line
