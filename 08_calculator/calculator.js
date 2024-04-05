b3d6b177d10044d12a792cb467cbfc3759e1369f
NathanSt10 <nstout@uwm.edu>
2024-04-05 00:06:19 -0500

    Completed eigth exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) return 0;
+	return arr.reduce((count, num) => count + num)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((count, num) => count * num)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+  let count = 1;
+	for (let i = 0; i < exponent; ++i) {
+    count = count * base;
+  }
+  return count;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let count = 1;
+  for (let i = 2; i <= num; ++i) {
+    count = count * i;
+  }
+  return count;
 };
 
 // Do not edit below this line
