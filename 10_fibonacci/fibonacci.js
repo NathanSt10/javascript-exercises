5452c6b950fb45bbb19181418614695b46dfc5f7
NathanSt10 <nstout@uwm.edu>
2024-04-05 01:01:48 -0500

    Completed tenth exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS";
+    if (n == 0) return 0;
+    let i = 1;
+    let x = 1;
+    let y = 0;
+    while (i < n) {
+        temp = x;
+        x = x + y;
+        y = temp;
+        ++i;
+    }
+    return x;
 };
 
 // Do not edit below this line
