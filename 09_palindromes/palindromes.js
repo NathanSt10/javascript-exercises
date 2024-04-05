fbaaec6e1cd142f0314232cd96854b2f55be0f7c
NathanSt10 <nstout@uwm.edu>
2024-04-05 01:02:42 -0500

    Completed nineth exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    let start = 0;
+    let end = string.length-1;
+    while (start <= end) {
+        let test = string[start++].toLowerCase()
+        let test2 = string[end--].toLowerCase()
+        if (test !== test2) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
