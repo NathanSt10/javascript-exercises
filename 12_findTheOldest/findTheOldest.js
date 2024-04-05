e24073464de6d3b95eb387f2859ab29928145f4c
NathanSt10 <nstout@uwm.edu>
2024-04-05 11:53:05 -0500

    Completed twelfth exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,7 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return oldest = people.reduce((oldestPerson, person) => {
+        return (person.yearOfDeath - person.yearOfBirth > oldestPerson.yearOfDeath - oldestPerson.yearOfBirth) ? person : oldestPerson;
+    });
 };
 
 // Do not edit below this line
